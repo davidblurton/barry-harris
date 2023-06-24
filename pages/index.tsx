f9c81bfad465fb76952aa0000dba30dfdb177e6f
David Blurton <david.blurton@gmail.com>
2023-06-24 22:25:46 +0000

    Display scale of chords

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import Piano from "@/components/Piano";
 import { Input } from "@/components/ui/input";
-import { Chord, Interval, Midi, Note } from "tonal";
-import { getChordWithOctave } from "@/lib/tonal";
+import { Chord, Midi, Note, Scale } from "tonal";
+import { getChordWithOctave, toDropTwo } from "@/lib/tonal";
 import { Slider } from "@/components/ui/slider";
 import { Chord as TChord } from "@tonaljs/chord";
-import { Json } from "@/components/Json";
+import { range } from "@/lib/functools";
 
 type DisplayChordProps = {
   inputText: string;
-  inversion: number;
 };
 
 function DisplayChord(props: DisplayChordProps) {
-  const { inputText, inversion } = props;
+  const { inputText } = props;
   const chord = Chord.get(inputText);
+  const [inversion, setInversion] = useState(0);
 
   if (chord.tonic === null) {
     return null;
@@ -43,24 +43,54 @@ function DisplayChord(props: DisplayChordProps) {
     return undefined;
   }
 
-  const diminished = getDiminished(chord);
+  function getScaleOfChords(chord: TChord) {
+    if (chord.tonic === null) {
+      return [];
+    }
 
-  const selection = {
-    "#f1ff5e": applyVoicing(chord),
-    "#78BEFF": diminished ? applyVoicing(diminished) : [],
-  };
+    if (chord.type === "sixth" || chord.type === "minor sixth") {
+      const root = Note.transpose(chord.tonic, "2M");
+      const diminished = Chord.get(root + "dim7");
+
+      const range = Scale.rangeOf([...chord.notes, ...diminished.notes]);
+      const scale = range(chord.tonic + "4", chord.tonic + "6");
+
+      return scale;
+    }
+
+    return [];
+  }
+
+  const scale = getScaleOfChords(chord);
+
+  const selections = range(8)
+    .map((inv) =>
+      toDropTwo([0, 1, 2, 3].map((i) => scale[i * 2 + inv]).filter(Boolean))
+        .map(Midi.toMidi)
+        .filter(Boolean)
+    )
+    .map((notes, i) => ({
+      [i % 2 == 0 ? "#F1FF5E" : "#78BEFF"]: notes,
+    }));
+
+  const selection = selections[inversion];
 
   return (
-    <div>
+    <div className="space-y-8 max-w-sm">
+      <Slider
+        max={selections.length - 1}
+        step={1}
+        value={[inversion]}
+        onValueChange={([value]) => setInversion(value)}
+      />
       <Piano selection={selection} />
-      {/* <Json obj={chord} /> */}
+      {/* <Json obj={selections} /> */}
     </div>
   );
 }
 
 export default function Index() {
   const [inputText, setInputText] = useState("C6");
-  const [inversion, setInversion] = useState(0);
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const value = e.target.value;
@@ -69,16 +99,11 @@ export default function Index() {
 
   return (
     <div className="px-8 py-8 space-y-8">
-      <Input placeholder="Chord" onChange={handleChange} value={inputText} />
+      <div className="space-y-8 max-w-sm">
+        <Input placeholder="Chord" onChange={handleChange} value={inputText} />
+      </div>
 
-      <Slider
-        max={3}
-        step={1}
-        value={[inversion]}
-        onValueChange={([value]) => setInversion(value)}
-      />
-
-      <DisplayChord inputText={inputText} inversion={inversion} />
+      <DisplayChord inputText={inputText} />
     </div>
   );
 }
